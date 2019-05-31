9d92c39f8a4b7512b6d720658b50400a999bfda3
rkrishan <ramkrishansharma1994@gmail.com>
2019-05-31 15:13:23 +0530

    Creation of ANgular4 project login and Reagistration tab is created

---
+++ specta/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MyMaterialModule } from  './material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule,Routes} from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MyMaterialModule,
+    RouterModule.forRoot([
+      {path:'',redirectTo:'/',pathMatch:'full'},
+      {path:'register',component:RegistrationComponent},
+      {path:'login',component:LoginComponent},
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
